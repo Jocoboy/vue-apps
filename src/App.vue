dccc6fe48657bfc09326db88bbde8effe3d659cd
Jocoboy <Jocoboy@outlook.com>
2024-10-13 01:48:44 +0800

    :wrench:Vite config.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
-<script setup></script>
-
 <template>
-  <div>hello</div>
+  <div>hello {{ num }}</div>
+  <button @click="click">click</button>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+let num = ref(0);
+
+function click() {
+  num.value++;
+}
+</script>
 
 <style scoped></style>
