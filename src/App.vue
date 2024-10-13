d26ff7b5003c7fdd2e5ed919c6059eac764d0c2b
Jocoboy <Jocoboy@outlook.com>
2024-10-14 00:44:21 +0800

    :sparkles:Add Element Plus.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <button @click="increment">按钮</button>
-  <h1>Bank Account Balance</h1>
-  <p>{{ $filters.currencyUSD(num) }}</p>
+  <el-button @click="increment">按钮</el-button>
+  <h1>Bank Account Balance : {{ $filters.currencyUSD(num) }}</h1>
+  <el-row class="mb-4">
+    <el-button @click="handleClick">Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+  <el-icon :size="100" color="red">
+    <Edit />
+  </el-icon>
   <!-- 路由出口 -->
   <!-- 路由匹配到的组件将渲染在这里 -->
   <router-view />
@@ -9,9 +19,20 @@
 
 
 <script setup>
+import { getCurrentInstance } from 'vue';
 import useCounter from './hooks/useCounter';
+import useSubmit from './hooks/useSubmit';
+
 
 const { num, doubles, increment } = useCounter();
+
+const { sexList, submitOk, submitFail, } = useSubmit();
+
+
+async function handleClick() {
+  submitOk('保存成功');
+  submitFail('操作失败');
+}
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 import { createApp } from 'vue';
-import '@/styles/index.scss';
 import App from './App.vue';
-import router from '@/router';
-import { filters } from '@/utils/filter.js';
+
+// global sass
+import '@/styles/index.scss';
+
 
 const app = createApp(App);
 
+// vue-router
+import router from '@/router';
 app.use(router);
 
+// global filters
+import { filters } from '@/utils/filter.js';
 app.config.globalProperties.$filters = filters;
+
+// element-plus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+app.use(ElementPlus);
+// 注册所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 app.mount('#app');
