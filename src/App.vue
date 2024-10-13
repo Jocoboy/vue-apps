942698c80da97691ee4c4aac645250a261de87cd
Jocoboy <Jocoboy@outlook.com>
2024-10-13 21:47:31 +0800

    :sparkles:Add vue-router.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <div>hello {{ num }}</div>
-  <button @click="click">click</button>
+  <h1>Hello App!</h1>
+  <!-- <h1>当前路由信息：{{ $route }}</h1> -->
+
+  <!--使用 router-link 组件进行导航 -->
+  <!--通过传递 `to` 来指定链接 -->
+  <ol>
+    <li><router-link to="/">Go to Home</router-link></li>
+    <li><router-link to="/test">Go to Test</router-link></li>
+  </ol>
+
+  <hr />
+
+  <!-- 路由出口 -->
+  <!-- 路由匹配到的组件将渲染在这里 -->
+  <router-view />
 </template>
 
+
 <script setup>
-import { ref } from 'vue';
 
-let num = ref(0);
-
-function click() {
-  num.value++;
-}
 </script>
 
 <style scoped></style>
