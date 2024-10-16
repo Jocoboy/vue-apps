d60d7ecd41252a646c1671c6ccfa4c1d6c161bbb
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2024-10-16 17:44:19 +0000

    Deploying to gh-pages from @ Jocoboy/vue-apps@29f414a295350470ba4e35e56022d0fd8599ced7 🚀

--- assets/index-Bm_TMXw7.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./index-BpdIav6V.js";const n={__name:"index",setup(r){return t.getss({}).then(e=>{}),(e,s)=>" 首页 "}};export{n as default};
---
+++ assets/index-qrJUBQVj.js
@@ -0,0 +1,1 @@
+import{m as t}from"./index-DeHcvzre.js";const _={__name:"index",setup(r){return t.test.getMapInfo().then(e=>{}),(e,s)=>" 首页 "}};export{_ as default};
