1619bfe04c468eaaab6e121e87904e83e74af341
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2024-10-20 18:17:26 +0000

    Deploying to gh-pages from @ Jocoboy/vue-apps@c32887d4f7f0296bab2544cdf527bf9a3b0fc3a4 🚀

---
+++ assets/404-DBsJ3-NP.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./index-Rji-9Wa8.js";const o={};function r(t,_){return n(),c("h1",null,"404")}const a=e(o,[["render",r]]);export{a as default};
---
+++ assets/index-DisS82u4.js
@@ -0,0 +1,1 @@
+import{o as e,c as t}from"./index-Rji-9Wa8.js";const a={__name:"index",setup(c){return(n,o)=>(e(),t("h1",null,"首页"))}};export{a as default};
